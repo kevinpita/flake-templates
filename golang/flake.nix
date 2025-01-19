eb95011387140452b3c5010ecde1ff6f0fc8ad68
Kevin Pita <gitkevin@pm.me>
2025-01-19 23:44:28 +0100

    feat: add gitignore & init git repo

--- golang/flake.nix
+++ golang/flake.nix
@@ -3,14 +3,24 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
+
+      gitignoreContent = ''
+        .direnv
+
+        .vscode/
+        .idea/
+      '';
     in
     {
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = with pkgs; [
+          git
+
           go
           gofumpt
           golangci-lint
@@ -20,8 +30,14 @@
           if [ ! -f go.mod ]; then
             go mod init "github.com/kevinpita/$(basename $(pwd))"
           fi
+
+          if [ ! -d .git ]; then
+            git init
+            echo "${gitignoreContent}" > .gitignore
+            git add .
+            git commit -m "Initial commit"
+          fi
         '';
       };
     };
 }
-
